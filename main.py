c3ccbeda0df4d8341f9f9852f7c9c6cb1117d36b
incorrect-horse <vincenso_h@yahoo.com>
2023-02-01 17:52:23 -0500

    added 'cred card' validation and payment class

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
-
 df = pd.read_csv("files/hotels.csv", dtype={"id": str})
-# df headers - id,name,city,capacity,available
+df_cards = pd.read_csv("files/cards.csv", dtype=str).to_dict(orient="records")
 
 
 class Hotel:
@@ -32,7 +31,7 @@ class Confirmation:
         content = f"""
         Thank you for your reservation!
         Here are your booking details:
-        Name: {self.guest_name}
+        Name: {self.guest_name.title()}
         Hotel: {self.hotel.name}
         City: {self.hotel.city}
         Reservation Number: ABC000123456789
@@ -40,13 +39,32 @@ class Confirmation:
         return content
 
 
+class CreditCard:
+    def __init__(self, number):
+        self.number = number
+
+    def validate(self, card_holder, expiration, cvc_code):
+        card_data = {"number": self.number,
+                     "expiration": expiration,
+                     "holder": card_holder,
+                     "cvc": cvc_code}
+        if card_data in df_cards:
+            return True
+
+
 print(df)
 hotel_id = input("Enter the id number of the hotel: ")
 hotel = Hotel(hotel_id)
 if hotel.view_available():
-    hotel.reserve()
-    guest_name = input("Enter guest name: ")
-    confirmation_number = Confirmation(guest_name=guest_name, hotel=hotel)
-    print(confirmation_number.generate())
+    credit_card = CreditCard(number="1234567890123456",)
+    if credit_card.validate(card_holder="JOHN SMITH",
+                            expiration="12/26",
+                            cvc_code="123"):
+        hotel.reserve()
+        guest_name = input("Enter guest name: ")
+        confirmation_number = Confirmation(guest_name=guest_name, hotel=hotel)
+        print(confirmation_number.generate())
+    else:
+        print("There was a problem with your payment.")
 else:
     print("No rooms availalbe in hotel.")
